0f3f6aace19a42ba67914c9cbc479c5beb4c1452
Jayden Pak <jppak86@gmail.com>
2022-02-07 17:29:14 -0500

    add confetti5

--- js/confetti.js
+++ js/confetti.js
@@ -1,4 +1,4 @@
-var confetti = {
+var confetti= {
 	maxCount: 500,	//set max confetti count
 	speed: 5,			//set the particle animation speed
 	frameInterval: 15,	//the confetti animation frame interval in milliseconds
